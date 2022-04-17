304265a2a4cbfddd4c3ec942b3c72e8e4eceef21
Lewis-Allen <lewisn11@hotmail.co.uk>
2022-04-17 16:19:51 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Razor Pages With Webpack
+Template repository for a .NET Razor Pages application with Webpack and Node.js.
+## Features:
+Template makes use of:
+* .NET 6
+* Node.js
+* Webpack
+* TypeScript
+* SCSS
+## Assets
+Assets are generated and placed into the wwwroot/dist/ folder.
+## Acknowledgements
+Favicon generated from a random image at [realfavicongenerator.net](https://realfavicongenerator.net/).
---
+++ Razor-Pages-With-Webpack/ClientApp/webpack.common.js
@@ -0,0 +1,48 @@
+﻿const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: {
+        site: ['./src/js/site.ts', './src/css/site.scss']
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, '..', 'wwwroot', 'dist')
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.(s?(a|c)ss)$/,
+                use: [{ loader: MiniCssExtractPlugin.loader }, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(eot|woff(2)?|ttf|otf|svg)$/i,
+                type: 'asset'
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    optimization: {
+        minimizer: [
+            '...',
+            new CssMinimizerPlugin(),
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: './favicon/' }
+            ]
+        })
+    ]
+};
